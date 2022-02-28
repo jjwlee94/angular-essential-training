8b0d71cf5f29e286c256d3fbc95535f3cfc19786
Jeewon Lee <jeewon.jlee@gmail.com>
2022-02-27 22:12:01 -0500

    Import HttpClientModule and HttpXhrBackend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HttpXhrBackend } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { MediaItemComponent } from "./media-item.component";
 import { MediaItemListComponent } from "./media-item-list.component";
@@ -8,9 +9,10 @@ import { FavoriteDirective } from "./favorite.directive";
 import { CategoryListPipe } from "./category-list.pipe";
 import { MediaItemFormComponent } from "./media-item-form.component";
 import { lookupListToken, lookupLists } from "./providers";
+import { MockXHRBackend } from "./mock-xhr-backend";
 
 @NgModule({
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
@@ -20,6 +22,9 @@ import { lookupListToken, lookupLists } from "./providers";
     MediaItemFormComponent,
   ],
   bootstrap: [AppComponent],
-  providers: [{ provide: lookupListToken, useValue: lookupLists }],
+  providers: [
+    { provide: lookupListToken, useValue: lookupLists },
+    { provide: HttpXhrBackend, useClass: MockXHRBackend },
+  ],
 })
 export class AppModule {}
