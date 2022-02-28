42b79ad01abaa34b6cf2f10ab90fa509cf35b402
Jeewon Lee <jeewon.jlee@gmail.com>
2022-02-27 23:41:28 -0500

    Import NewItemModule to app module

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,9 @@
 import { Routes, RouterModule } from "@angular/router";
-import { MediaItemFormComponent } from "./new-item/media-item-form.component";
 import { MediaItemListComponent } from "./media-item-list.component";
+
 const appRoutes: Routes = [
-  { path: "add", component: MediaItemFormComponent },
   { path: ":medium", component: MediaItemListComponent },
-  { path: "", redirectTo: "all", pathMatch: "full" },
+  { path: "", pathMatch: "full", redirectTo: "all" },
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
